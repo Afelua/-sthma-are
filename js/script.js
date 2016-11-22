70960ec869bbe67df959fa6651b53185019533b6
Afelua <elena.semenova_2012@mail.ru>
2016-11-22 19:49:50 +0300

    Some fixed

--- js/script.js
+++ js/script.js
@@ -38,6 +38,7 @@ jQuery(function($) {
   // ==========================================================================
   // Animate menu
   // ==========================================================================
+
   var $titleEls = $('strong', $sidedrawerEl);
   
   $titleEls
@@ -47,4 +48,130 @@ jQuery(function($) {
   $titleEls.on('click', function() {
     $(this).next().slideToggle(200);
   });
+
+  $('#edit').on('click', editUserInfo);
+
+  function editUserInfo() {
+    $( '.mui-table--information input' ).removeAttr( "disabled" ).addClass('input--white');
+    $('#edit').hide();
+    $('#save').show();
+  }
+
+  $('#save').click(function(){
+    // собираем данные с формы
+    var fname 	 = $('#fname').val();
+    var name 	 = $('#name').val();
+    var sname = $('#sname').val();
+    var b_date	 = $('#b_date').val();
+    var growth 	 = $('#growth').val();
+    var address = $('#address').val();
+    var address_last 	 = $('#address_last').val();
+    var phone = $('#phone').val();
+
+    $.ajax({
+      url: "action.php",
+      type: "post",
+      dataType: "json",
+      data: {
+        "fname": 	fname,
+        "name": 	name,
+        "sname": sname,
+        "b_date": 	b_date,
+        "growth": growth,
+        "address": address,
+        "address_last": address_last,
+        "phone": phone
+      },
+
+      success: function(data){
+        $('.messages').html('Информация успешно обновлена');
+        $('#save').hide();
+        $('#edit').show();
+      }
+    });
+  });
+
+
+  $('#add_parameter').on('click', addNewInfo);
+
+  function addNewInfo() {
+    $('.add_show').show();
+    $('#add_parameter').hide();
+  }
+
+  $('#send_parameter').click(function(){
+    var date	 = $('#date').val();
+    var result	 = $('#result').val();
+
+    $.ajax({
+      url: "action_parameter.php",
+      type: "post",
+      dataType: "json",
+      data: {
+        "date": 	date,
+        "result": 	result
+      },
+
+      success: function(data){
+        $('.messages').html('Информация успешно обновлена'); // выводим ответ сервера
+        $('#send_parameter').hide();
+        $('#add_parameter').show();
+      }
+    });
+  });
+
+
+  $('#send-new-user').click(function(){
+    // собираем данные с формы
+    var fname 	 = $('#fname').val();
+    var name 	 = $('#name').val();
+    var sname =  $('#sname').val();
+    var b_date	 = $('#b_date').val();
+    var growth 	 = $('#growth').val();
+    var sex = $('#sex').val();
+
+
+    $.ajax({
+      url: "action_new-user.php",
+      type: "post",
+      dataType: "json",
+      data: {
+        "fname": 	fname,
+        "name": 	name,
+        "sname": sname,
+        "b_date": 	b_date,
+        "growth": growth,
+        "sex": sex
+      },
+
+      success: function(data){
+        $('.messages').html('Информация успешно обновлена');
+        $('#save').hide();
+        return false;
+      }
+    });
+  });
+
+
+  $('#login_button').click(function(){
+    var login	 = $('#login').val();
+    var password	 = $('#password').val();
+
+    $.ajax({
+      url: "authorization.php",
+      type: "post",
+      dataType: "json",
+      data: {
+        "login": 	login,
+        "password": 	password
+      },
+
+      success: function(data){
+        top.location.href="index.html";
+      }
+    });
+  });
+
+  $('#tablePatients').DataTable();
+
 });
